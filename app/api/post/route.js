7b1c09661dbaa98453a9b3b40515dc967d08f962
Arek Cios <arek123113@gmail.com>
2024-02-13 17:47:59 +0100

    Fix: Discord webhooks allows only messages shorter than 2000 characters

--- app/api/post/route.js
+++ app/api/post/route.js
@@ -1,23 +1,49 @@
 import { NextResponse } from "next/server";
 
+function splitText(text, chunkSize = 2000) {
+    const chunks = [];
+    for (let i = 0; i < text.length; i += chunkSize) {
+        chunks.push(text.substring(i, i + chunkSize));
+    }
+    return chunks;
+}
+
 export async function POST(request) {
-    const res = await request.json();
-
     const webhookUrl = process.env.WEBHOOK_URL;
+    const res = await request.text();
+    const text = JSON.stringify(res);
 
-    console.log(res);
+    if (text.length < 2000) {
+        const body = { content: text };
 
-    const body = { content: JSON.stringify(res) };
+        const response = await fetch(webhookUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        });
+    } else {
+        const chunks = splitText(text);
 
-    const response = await fetch(webhookUrl, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-    });
+        chunks.forEach((chunk) => {
+            const body = { content: chunk };
 
-    //console.log(response);
+            const response = fetch(webhookUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body),
+            });
+        });
+    }
+
+    //since discord webhook content max legth is 2000 characters
+    //gotta split greater messages.
+
+    //const webhookResponse = await response.text();
+    //console.log(webhookResponse);
 
     return NextResponse.json({ res }, { status: 200 });
 }
